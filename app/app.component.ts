335e8192fcf967ec95232ae8dc40371e16429333
Hamid Alizadeh <silvanatnk.com@gmail.com>
2017-01-15 18:13:00 +0330

    enhance app component to use services and promise

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -63,14 +65,25 @@ import { Hero } from './hero';
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
-
+  `],
+  providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heros';
-  heroes = HEROES;
+  //heroes = Hero[];
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
 @Injectable()
 export class HeroService {
 
-    getHeros(): void {}
+    getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+    }
 }
